759b4df6ef81705d5c8afe897e05f44ad58c4b2f
Alec Lowry <accounts@aleclowry.com>
2025-02-11 18:29:38 +0000

    build: Setup project scaffolding

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }:
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+        packages.${system}.default = 
+            pkgs.writeShellApplication {
+                name = "tmux-session-manager";
+                runtimeInputs = [];
+                text = builtins.readFile ./zellij-session-manager.sh;
+            };
+
+
+        devShell.x86_64-linux =
+            pkgs.mkShell {
+                buildInputs = with pkgs;[
+                    nil
+                    bash-language-server
+                ];
+            };
+    };
+}
